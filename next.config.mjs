68048ec5a35190432e3bae9bd208791b039446c4
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2026-07-06 22:21:23 +0530

    chore: setup .gitignore, apply edge warning fix, remove root schema

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,16 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config, { webpack, nextRuntime }) => {
+    if (nextRuntime === "edge") {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          "process.version": JSON.stringify("v22.11.0"),
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
